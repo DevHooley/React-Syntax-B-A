2214b5593236dde97ded4ee1b1600627256d2b3d
Your Name <92824573+kyleehooley@users.noreply.github.com>
2023-04-06 14:44:59 -0500

    latest

--- src/components/Homepage/HomePage.tsx
+++ src/components/Homepage/HomePage.tsx
@@ -1,18 +1,17 @@
-import CalendarApp from "../CalendarApp/CalendarApp";
-import ModeToggler from "./ModeToggler";
-import HelloWorld from "./HelloWorld";
-import LittleLemon from "./LittleLemon";
 import Header from "./Header";
 import Main from "./Main";
 import Sidebar from "./Sidebar";
 import Stateless from "./Stateless";
+import Work from "../../assets/Images/work.jpg";
 
 const Card = "grid grid-cols-2 p-4 shadow-lg shadow-gray-300 rounded-md";
+const work = "h-100 mx-auto shadow-lg";
 
 function HomePage() {
   return (
     <div>
       <h1 className="">Welcome to my site</h1>
+      <img src={Work} alt="it ain't much, but its honest work" />
       <div className="h-auto w-auto p-10">
         <Header name="Anna" color="purple" />
         <div className={Card}>
